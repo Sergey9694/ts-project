d4000a86562cdf65529e6124bfc9fae932bcbdb3
Sergey Drobilenko <drobilenko94@mail.ru>
2023-05-21 20:15:03 +0300

    фетчинг с axios (типизация хуков и axios)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
+import { useEffect, useState } from "react";
 import Card, { CardVariant } from "./components/Card";
 import UsersList from "./components/UsersList";
 import { IUser } from "./types/types";
+import axios from "axios";
 
 const App = () => {
-    const users: IUser[] = [
-        {
-            id: 1,
-            name: "Leanne Graham",
-            email: "Sincere@april.biz",
-            address: {
-                street: "Kulas Light",
-                city: "Gwenborough",
-                zipcode: "92998-3874",
-            },
-        },
-        {
-            id: 2,
-            name: "Ervin Howell",
-            email: "Shanna@melissa.tv",
-            address: {
-                street: "Victor Plains",
-                city: "Wisokyburgh",
-                zipcode: "0566-7771",
-            },
-        },
-    ];
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>(
+                "https://jsonplaceholder.typicode.com/users"
+            );
+            setUsers(response.data);
+        } catch (error) {
+            alert(error);
+        }
+    }
 
     return (
         <div>
